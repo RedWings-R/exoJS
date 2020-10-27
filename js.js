f866d1729ba376cb1bc6a5fabe5367fc3428bd81
RedWings-R <yves.henot01@gmail.com>
2020-10-27 12:57:36 +0100

    ok

--- js.js
+++ js.js
@@ -14,25 +14,20 @@ document.querySelector("#btnStart").addEventListener("click", () => {
     if(etatPArtie === true){
         tabP1 = [];
         tabP2 = [];
-
-        b1.style.backgroundImage = "none";
-        b2.style.backgroundImage = "none";
-        b3.style.backgroundImage = "none";
-        b4.style.backgroundImage = "none";
-        b5.style.backgroundImage = "none";
-        b6.style.backgroundImage = "none";
-        b7.style.backgroundImage = "none";
-        b8.style.backgroundImage = "none";
-        b9.style.backgroundImage = "none";
-        
+        document.querySelectorAll('.wrapper>span').forEach(elem => elem.style.backgroundImage = "none"); 
     }else{
         document.querySelector("#btnStart").innerHTML = "Relancer la partie"
-        document.querySelector("#plateau").style.display = "block";
+        document.querySelector("#divPlateau").style.display = "block";
         etatPArtie = true;
+        for(let i = 1; i < 10 ; i++ ){
+            var x = document.createElement("SPAN");
+            x.id = String(i);
+            var t = document.createTextNode("This is a span element.");
+            x.appendChild(t);
+            document.querySelector("#plateau").appendChild(x);
+        } 
     }
-
 })
-
 
 const croixOUrond = () => {
     if(playerTurn === true){
@@ -44,16 +39,6 @@ const croixOUrond = () => {
 }
 
 
-document.querySelector("#b1").addEventListener("click", croixOUrond());
-document.querySelector("#b2").addEventListener("click", croixOUrond());
-document.querySelector("#b3").addEventListener("click", croixOUrond());
-document.querySelector("#b4").addEventListener("click", croixOUrond());
-document.querySelector("#b5").addEventListener("click", croixOUrond());
-document.querySelector("#b6").addEventListener("click", croixOUrond());
-document.querySelector("#b7").addEventListener("click", croixOUrond());
-document.querySelector("#b8").addEventListener("click", croixOUrond());
-document.querySelector("#b9").addEventListener("click", croixOUrond());
-
-
 // b1.style.backgroundImage = "url('rond.png')";
 //     b1.style.backgroundSize = "contain";
+
